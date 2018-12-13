c95106aa368151cf4d60077f28defa092f60058d
Nazanin <shambayati.nazanin@gmail.com>
2018-12-13 09:20:27 -0800

    Update ex_21NOV_2.py

--- ex_21NOV_2.py
+++ ex_21NOV_2.py
@@ -1,6 +1,6 @@
 #!/Library/Frameworks/Python.framework/Versions/3.6/bin/python3
 
-# opening the file "employee" and printing the number of lines in that file using 'for in' function
+# opening the file "employee" and printing the number of lines in that file using 'for' function
 sum=0
 with open("employee","r") as f:
 	for line in f:
