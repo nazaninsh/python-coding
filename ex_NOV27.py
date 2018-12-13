8e74a0ce81019304b15b1decfea940104bae3dd5
Nazanin <shambayati.nazanin@gmail.com>
2018-12-13 12:57:58 -0800

    Update ex_NOV27.py

--- ex_NOV27.py
+++ ex_NOV27.py
@@ -5,7 +5,6 @@ with open("DNA.txt","r") as a:
         print(line.rstrip())
 #read lines from the begining and store them in the list called "lines"
     a.seek(0)
-
     lines=a.readlines()
     print(lines)
 # read 100 charactors begining from 50th charactor in the DNA.txt files
