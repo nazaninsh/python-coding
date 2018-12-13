0e7ec11bcc7a622dc401d698e1814e066e84c188
Nazanin <shambayati.nazanin@gmail.com>
2018-12-12 21:24:42 -0800

    Update ex_21NOV_1.py

--- ex_21NOV_1.py
+++ ex_21NOV_1.py
@@ -1,11 +1,11 @@
 #!/Library/Frameworks/Python.framework/Versions/3.6/bin/python3
-
+#importing os library  
 import os
-
+#we changed directory to /practice with os.chdir(" ")
 os.chdir("/Users/nazaninshambayati/practice")
-
+#get the current working directory plus adding new line after printing that
 a=os.getcwd()
 print(a,'\n')
-
+#listing current directories plus adding a new line afterwards
 b=os.listdir()
 print(b,'\n')
